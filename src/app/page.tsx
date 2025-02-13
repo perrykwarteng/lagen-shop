f52f8b8b285d132f3e3a5aa7dff51c5f039cac0d
Perry John Kwarteng <perrykwarteng8@gmail.com>
2025-02-13 17:33:36 +0000

    worked on profile toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Footer } from "@/components/Footer/page";
 import { Heading } from "@/components/Heading/Heading";
 import { Hero } from "@/components/Hero/page";
@@ -5,14 +7,21 @@ import { NavBar } from "@/components/NavBar/page";
 import { NewsLetterForm } from "@/components/NewsLetterForm/pasge";
 import { ProductCard } from "@/components/ProductCard/page";
 import { SubscriptionBanner } from "@/components/SubscriptionBanner/page";
+import Link from "next/link";
+import { useState } from "react";
 
 export default function Home() {
+  const [showProfile, setShowProfile] = useState(false);
+  const handleShowProfile = () => {
+    setShowProfile(!showProfile);
+    console.log(showProfile);
+  };
   return (
-    <>
-      <NavBar />
-      <main>
+    <div>
+      <NavBar showProfile={handleShowProfile} />
+      <main className="relative">
         <Hero />
-        <div className="px-5 md:px-10 py-5 md:py-10">
+        <div className="px-5 md:px-10 py-5 md:py-10 ">
           <Heading heading={"Trending"} />
           <div className="w-full flex items-center justify-center flex-wrap gap-4 mt-8">
             <div className="">
@@ -37,6 +46,49 @@ export default function Home() {
         </div>
       </main>
       <Footer />
-    </>
+
+      <div
+        className={
+          showProfile
+            ? "w-[150px] bg-white list-none p-4 border-2 border-black absolute right-0 top-28"
+            : "hidden"
+        }
+      >
+        <li className="">
+          <Link
+            href="/login"
+            className="text-[14px] font-[500]  hover:text-gray-700 cursor-pointer"
+          >
+            Login
+          </Link>
+        </li>
+
+        <li className="">
+          <Link
+            href="/register"
+            className="text-[14px] font-[500]  hover:text-gray-700 cursor-pointer"
+          >
+            Register
+          </Link>
+        </li>
+
+        <li className="">
+          <Link
+            href="/"
+            className="text-[14px] font-[500]  hover:text-gray-700 cursor-pointer"
+          >
+            Account
+          </Link>
+        </li>
+        <li className="">
+          <Link
+            href="/faqs"
+            className="text-[14px] font-[500]  hover:text-gray-700 cursor-pointer"
+          >
+            FAQs
+          </Link>
+        </li>
+      </div>
+    </div>
   );
 }
