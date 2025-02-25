a2d56f4aa6ea0e975bcba2ae9d9e0c31c8c7a055
Perry John Kwarteng <perrykwarteng8@gmail.com>
2025-02-25 12:52:42 +0000

    collection page

--- src/app/(pages)/collections/page.tsx
+++ src/app/(pages)/collections/page.tsx
@@ -1,3 +1,11 @@
+import Layout from "@/components/Layout/layout";
+
 export default function Collections() {
-  return <div>Collections Page</div>;
+  return (
+    <>
+      <Layout>
+        <div className="px-5 md:px-10 py-5 md:py-10">Collections</div>
+      </Layout>
+    </>
+  );
 }
