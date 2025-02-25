335e85b04377a05218e56114d3794a7fde29c820
Perry John Kwarteng <perrykwarteng8@gmail.com>
2025-02-25 12:51:02 +0000

    fixed product card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,43 @@ import { Heading } from "@/components/Heading/Heading";
 import { ProductCard } from "@/components/ProductCard/page";
 import { NewsLetterForm } from "@/components/NewsLetterForm/pasge";
 import { SubscriptionBanner } from "@/components/SubscriptionBanner/page";
+import PictureOne from "../../public/images/picture1.png";
 
 export default function Home() {
   return (
     <Layout>
       <Hero />
       <div className="px-5 md:px-10 py-5 md:py-10">
-        <Heading heading={"Trending"} />
-        <div className="w-full flex items-center justify-center flex-wrap gap-4 mt-8">
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+        <Heading heading={"Trending"} text="" />
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-14">
+          <ProductCard
+            image={PictureOne}
+            title="Willow Wrap Sweatshirt"
+            color="Beige"
+            price="57.43"
+            sizes={["S", "M", "L", "XL", "XXL"]}
+          />
+          <ProductCard
+            image={PictureOne}
+            title="Willow Wrap Sweatshirt"
+            color="Beige"
+            price="57.43"
+            sizes={["S", "M", "L", "XL", "XXL"]}
+          />
+          <ProductCard
+            image={PictureOne}
+            title="Willow Wrap Sweatshirt"
+            color="Beige"
+            price="57.43"
+            sizes={["S", "M", "L", "XL", "XXL"]}
+          />
+          <ProductCard
+            image={PictureOne}
+            title="Willow Wrap Sweatshirt"
+            color="Beige"
+            price="57.43"
+            sizes={["S", "M", "L", "XL", "XXL"]}
+          />
         </div>
       </div>
       <div className="my-4">
