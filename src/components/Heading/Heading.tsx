fb5f344925d06e7630954e268c7ce54b859e0720
Perry John Kwarteng <perrykwarteng8@gmail.com>
2025-02-26 06:34:32 +0000

    updated the header component with a subtext

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,8 +1,15 @@
-export const Heading = ({ heading }: { heading: string }) => {
+export const Heading = ({
+  heading,
+  text,
+}: {
+  heading: string;
+  text: string;
+}) => {
   return (
     <div className="flex flex-col items-center justify-center">
       <h2 className="text-[30px] font-semibold uppercase ">{heading}</h2>
       <div className="w-[60px] h-1 bg-black rounded-md"></div>
+      <p className="mt-4 text-center">{text}</p>
     </div>
   );
 };
