aa883df022af00e53dabf60bda188209ed4022af
Perry John Kwarteng <perrykwarteng8@gmail.com>
2025-02-27 13:55:12 +0000

    added link to campaigns

--- src/components/NavBar/page.tsx
+++ src/components/NavBar/page.tsx
@@ -112,9 +112,13 @@ export const NavBar = ({
           </Link>
         </li>
 
-        <li className={pathName == "" ? "active border-b-2 border-black" : ""}>
+        <li
+          className={
+            pathName == "/campaigns" ? "active border-b-2 border-black" : ""
+          }
+        >
           <Link
-            href=""
+            href="/campaigns"
             className="text-[10px] md:text-[12px] font-[500]  hover:text-gray-700 cursor-pointer"
           >
             LOOKBOOK
