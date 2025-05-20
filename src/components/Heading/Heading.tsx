5c527bfce2ba156cb23d6948bc6bd7a57fcf1ff3
Perry John Kwarteng <perrykwarteng8@gmail.com>
2025-05-20 02:01:09 +0000

    added a secondary heading

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,15 +1,18 @@
 export const Heading = ({
   heading,
   text,
+  heading2,
 }: {
   heading: string;
   text: string;
+  heading2?: string;
 }) => {
   return (
     <div className="flex flex-col items-center justify-center">
       <h2 className="text-[30px] font-semibold uppercase ">{heading}</h2>
       <div className="w-[60px] h-1 bg-black rounded-md"></div>
-      <p className="mt-4 text-center">{text}</p>
+      <p className="mt-4 font-medium text-[20px]">{heading2}</p>
+      <p className="mt-2 text-center">{text}</p>
     </div>
   );
 };
