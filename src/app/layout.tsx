be17a4a516ccc0668f96305dc98283b6ecb3e2cb
Perry John Kwarteng <perrykwarteng8@gmail.com>
2025-08-20 01:59:56 +0000

    imported toaster and authContext

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
+import { AuthProvider } from "@/context/AuthContext";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -20,7 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="min-h-screen">{children}</main>
+        <AuthProvider>
+          <main className="min-h-screen">{children}</main>
+          <Toaster position="top-right" />
+        </AuthProvider>
       </body>
     </html>
   );
